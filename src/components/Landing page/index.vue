e9051b6b77d6da0ae7d7fd834491161babe0f4be
Martina Magdi <martinamagdi6@gmail.com>
2021-12-27 06:06:49 +0200

    Finishing task

---
+++ src/components/Landing page/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="landing-page">
+    <SectionOne />
+    <SectionTwo />
+    <SectionThree />
+  </div>
+</template>
+
+<script>
+import SectionOne from "./SectionOne.vue";
+import SectionTwo from "./SectionTwo.vue";
+import SectionThree from "./SectionThree.vue";
+
+export default {
+  name: "LandingPage",
+  components: { SectionOne, SectionTwo, SectionThree },
+};
+</script>
---
+++ src/components/Layouts/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="footer">
+    <div>
+      <img src="../../assets/images/zid-logo-2.png" alt="logo" />
+      <ul class="social-container">
+        <li class="social-item">
+          <a>
+            <v-icon>mdi-twitter</v-icon>
+          </a>
+        </li>
+        <li class="social-item">
+          <a>
+            <v-icon>mdi-instagram</v-icon>
+          </a>
+        </li>
+        <li class="social-item">
+          <a>
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+        </li>
+        <li class="social-item">
+          <a>
+            <v-icon>mdi-youtube</v-icon>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="buttons-container">
+      <v-icon class="message-icon">mdi-forum</v-icon>
+      <v-icon class="arrow-icon">mdi-arrow-up</v-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Footer',
+
+    data: () => ({
+    }),
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/styles/variables";
+@import "../../assets/styles/Layouts/Footer";
+</style>
